4eeaee261325d0c2bd366bb33374ff69235e77f3
masayuki-ioki <masayuki-ioki@cookpad.com>
2022-05-17 12:51:30 +0900

    :+1: Allow to set Time for timepicker

--- lib/slack_valid_block_kit/builder/elements.rb
+++ lib/slack_valid_block_kit/builder/elements.rb
@@ -202,7 +202,13 @@ module SlackValidBlockKit
         hash = { type: 'timepicker' }
         hash[:action_id] = action_id
         hash[:placeholder] = placeholder unless placeholder.nil?
-        hash[:initial_time] = initial_time unless initial_time.nil?
+        unless initial_time.nil?
+          if initial_time.is_a?(Time)
+            hash[:initial_time] = initial_time.strftime("%H:%M")
+          else
+            hash[:initial_time] = initial_time
+          end
+        end
         hash[:confirm] = confirm unless confirm.nil?
         hash[:focus_on_load] = focus_on_load unless focus_on_load.nil?
         hash
